33ada3f87f3ff3bcfe2b3bd81c3a9e2699c3c249
YubalHormiga <yubalhh@gmail.com>
2023-07-20 19:59:12 +0200

    update project and started TasteTrove project

---
+++ Proyectos Vue/3-TasteTrove/src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+</script>
+<template>
+  <header>
+    <div>
+      <h1>Taste<span>Trove</span></h1>
+    </div>
+    <nav>
+      <RouterLink :to="{ name: 'home' }" class="link"> Incio </RouterLink>
+      <RouterLink :to="{ name: 'collection' }"> Colección </RouterLink>
+    </nav>
+  </header>
+</template>
+<style>
+.link{
+    color:red
+}
+</style>
---
+++ Proyectos Vue/3-TasteTrove/src/views/CulinaryCollection.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+    <div>
+        <h1>Hola desde Collection</h1>
+    </div>
+</template>
