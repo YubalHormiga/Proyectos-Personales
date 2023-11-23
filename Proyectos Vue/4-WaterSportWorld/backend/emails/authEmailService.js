d4e8514cf5c7acf14dcae5d204e89c6bd114838b
YubalHormiga <yubalhh@gmail.com>
2023-11-23 19:38:14 +0100

    environmental variables

--- Proyectos Vue/4-WaterSportWorld/backend/emails/authEmailService.js
+++ Proyectos Vue/4-WaterSportWorld/backend/emails/authEmailService.js
@@ -1,17 +1,17 @@
 import { createTransport } from '../config/nodemailer.js'
 
 export async function sendEmailVerification({ name, email, token }) {
-    const transporter = createTransport("sandbox.smtp.mailtrap.io", 2525, "4df2bd27a759e3", "df744c43faab98")
+    const transporter = createTransport(process.env.EMIAL_HOST, process.env.EMIAL_PORT, process.env.EMAIL_USER, process.env.EMAIL_PASSWORD)
 
 
     const info = await transporter.sendMail({
-        from: 'SWS',
+        from: 'SWS <correo@watersportworld.com',
         to: 'correo@correo.com',
         subject: 'SWS - Confirma cuenta',
         text: ' SWS - Confirma tu cuenta',
         html: `<p>Hola: ${name}, confirma tu cuenta en AppSalon</p>
         <p>Tu cuenta esta casi lista, solo debes confirmarla en el siguiente enlace:</p>
-        <a href="http://localhost:4000/api/auth/verify/${token}">Confirmar cuenta</a>
+        <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}">Confirmar cuenta</a>
         <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>`
     })
 }
